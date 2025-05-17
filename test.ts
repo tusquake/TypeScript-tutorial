2eb1a54be50ff3b56dc7ac77f39df4e78b564c75
Your Name <you@example.com>
2025-05-17 12:02:43 +0530

    Add initial TypeScript files and configuration

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let oddNumbers = numbers.filter((num) => num % 2 !== 0);
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let oddNumber = numbers.filter((num) => num % 2 !== 0);
 
 console.log(oddNumbers);
---
+++ user.ts
@@ -0,0 +1,3 @@
+var age: number = 20;
+let names: string = "Tushar";
+console.log("Hello " + names + " " + age);
